1b6fbdb9d1d828c18f849b326879db2554243c92
Sam Reskala <sreskala@outlook.com>
2024-07-31 22:39:41 -0400

    adding simple animation to landing

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,30 @@
-import { useState } from 'react'
 import { Link } from 'react-router-dom'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Lottie from "react-lottie";
+//@ts-ignore
+import spaceship from './assets/spaceship.json'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: spaceship,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice"
+    }
+  };
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <Lottie 
+	    options={defaultOptions}
+        height={400}
+        width={400}
+      />
       </div>
       <h1>Welcome to Julian Planets!</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
     <Link to="/planets">Click to Launch</Link>
     </>
   )
